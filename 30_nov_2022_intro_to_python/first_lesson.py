57797c43600c71efcb1d22faef5075a2c71efbf2
AlimAlb <alimalbogachiev@yandex.ru>
2022-11-28 23:37:48 +0300

    re

---
+++ 30_nov_2022_intro_to_python/first_lesson.py
@@ -0,0 +1,46 @@
+#понятие переменной
+s = 'asdasda'
+print(hex(id(s)))
+#строки
+s = 'abcd'
+print(s)
+s2 = 'cvbcvb'
+s3 = s+s2
+print(s3)
+s4 = s3*5
+#ввод данных
+hi = 'Hello, '
+name = input()
+out = hi + name
+print(out)
+#целые числа
+a = 17
+b = 10
+c = a+b
+print(c)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#перевод часов в секунду
+#вещественные числа
+a = 17.0
+b = 10.0
+c = a+b
+print(c)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#посчитать составные выражение
+#Логические выражения
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not(a))
+#задачка про попадание числа в промежуток
